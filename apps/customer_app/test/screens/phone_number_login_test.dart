bad3a94101ab1c679e1227c172fff1f3baa604fa
Abin Babu <abinbabuabk@gmail.com>
2024-02-19 21:29:20 +0530

    Authentication and testcase fixes

--- apps/customer_app/test/screens/phone_number_login_test.dart
+++ apps/customer_app/test/screens/phone_number_login_test.dart
@@ -1,17 +1,47 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:common/widgets/primary_button.dart';
+import 'package:customer_app/blocs/auth_bloc/auth_bloc.dart';
+import 'package:customer_app/blocs/auth_bloc/auth_state.dart';
 import 'package:customer_app/constants/labels.dart';
 import 'package:customer_app/screens/phone_number_login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../mocks/firebase_mock.dart';
+import '../mocks/mocks.dart';
+
 void main() {
+
+  late MultiBlocProvider widget;
+
+  setupFirebaseAuthMocks();
+
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+
+  setUp(() {
+    MockAuthBloc authBloc = MockAuthBloc();
+    widget = MultiBlocProvider(
+      providers: [BlocProvider<AuthBloc>.value(value: authBloc)],
+      child: MaterialApp(home: PhoneNumberLoginScreen()),
+    );
+
+    whenListen(
+        authBloc,
+        Stream.fromIterable([AuthInitialState()]),
+        initialState:  AuthInitialState()
+    );
+  });
+
+
   testWidgets('PhoneNumberLoginWidget renders correctly',
       (WidgetTester tester) async {
     // Build our widget and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
-      home: PhoneNumberLoginScreen(),
-    ));
+    await tester.pumpWidget(widget);
 
     // Verify that the text 'Phone Number' is rendered.
     expect(find.text(Labels.PHONE_NUMBER), findsOneWidget);
@@ -32,9 +62,7 @@ void main() {
   testWidgets('PhoneNumberLoginWidget System UI Overlay Test',
       (WidgetTester tester) async {
     // Build our widget and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
-      home: PhoneNumberLoginScreen(),
-    ));
+    await tester.pumpWidget(widget);
 
     // Verify that System UI Overlay Style is set for iOS
     const systemUiOverlayStyle = SystemUiOverlayStyle(
