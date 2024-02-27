fc003812d68a97643ce5eeecb4a4cba195be1a0d
Abin Babu <abinbabuabk@gmail.com>
2024-02-28 00:26:11 +0530

    freezed

---
+++ apps/customer_app/lib/model/generated/category.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../category.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CategoryImpl _$$CategoryImplFromJson(Map<String, dynamic> json) =>
+    _$CategoryImpl(
+      name: json['name'] as String?,
+      description: json['description'] as String?,
+      image: json['image'] as String?,
+      id: json['id'] as String?,
+    );
+
+Map<String, dynamic> _$$CategoryImplToJson(_$CategoryImpl instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'description': instance.description,
+      'image': instance.image,
+      'id': instance.id,
+    };
